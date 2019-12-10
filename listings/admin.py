6da45663b1e9959830a7c8032e8ff791cce1d9ba
Timothy Cao <tcao.pro@gmail.com>
2019-12-10 13:04:10 -0500

    admin customization

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'neighborhood', 'price', 'is_published', 'list_date', 'realtor')
+  list_display_links = ('id', 'title')
+  list_filter = ('realtor', 'neighborhood')
+  list_editable = ('is_published',)
+  search_fields = ('title', 'neighborhood', 'address', 'city', 'state', 'zipcode', 'description')
+  list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
--- metro/settings.py
+++ metro/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'metro/static')
 ]
+
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
