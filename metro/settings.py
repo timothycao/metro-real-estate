a8a963f3b64a6eeef4a99e1d42b8461b5f5e13cc
Timothy Cao <tcao.pro@gmail.com>
2019-12-14 19:55:33 -0500

    contacts app, model, and admin customization

--- metro/settings.py
+++ metro/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig'
 ]
 
 MIDDLEWARE = [
