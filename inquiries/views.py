332b6aed76555126a402a1867ae9bb233d903338
Timothy Cao <tcao.pro@gmail.com>
2019-12-14 21:19:08 -0500

    contacts renamed to inquries, and updated inquiry form

--- inquiries/views.py
+++ inquiries/views.py
@@ -13,6 +13,14 @@ def inquiry(request):
     user_id = request.POST['user_id']
     realtor_email = request.POST['realtor_email']
 
+    # Check if user has already made an inquiry for the listing
+    if request.user.is_authenticated:
+      user_id = request.user.id
+      has_inquired = Inquiry.objects.all().filter(listing_id=listing_id, user_id=user_id)
+      if has_inquired:
+        messages.error(request, 'You have already made an inquiry for this listing.')
+        return redirect('/listings/' + listing_id)
+
     inquiry = Inquiry(listing_id=listing_id, listing=listing, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
     inquiry.save()
