1847314992614bb6f34808eb8f2051dc959f8d85
Timothy Cao <tcao.pro@gmail.com>
2019-12-15 20:21:57 -0500

    listing title updates

--- listings/admin.py
+++ listings/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title', 'neighborhood', 'price', 'is_published', 'list_date', 'realtor')
+  list_display = ('id', 'title', 'bedrooms', 'bathrooms', 'price', 'is_published', 'list_date', 'realtor')
   list_display_links = ('id', 'title')
-  list_filter = ('realtor', 'neighborhood')
+  list_filter = ('realtor',)
   list_editable = ('is_published',)
   search_fields = ('title', 'neighborhood', 'address', 'city', 'state', 'zipcode', 'description')
   list_per_page = 25
