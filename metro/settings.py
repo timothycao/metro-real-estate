ced860e5398b4b11161b8f2b885fdfa35491aee7
Timothy Cao <tcao.pro@gmail.com>
2019-12-23 11:28:37 -0500

    heroku deployment

--- metro/settings.py
+++ metro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +139,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Heroku
+django_heroku.settings(locals())
